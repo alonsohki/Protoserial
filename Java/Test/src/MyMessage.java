2f670baf33a52e6a25aae80fe97e47846b8e5440
Alberto Alonso <rydencillo@gmail.com>
2013-05-19 20:19:56 +0300

    Added the serialization methods for Java.

--- Java/Test/src/MyMessage.java
+++ Java/Test/src/MyMessage.java
@@ -4,15 +4,19 @@ import com.conditionracer.protoserial.*;
 public class MyMessage extends ParentMessage
 {
     public Integer varIntegerNullable;
-    public Integer varInteger;
-    public Long varLong;
+    @Unsigned public Short varUshort;
+    public Short varShort;
     @Unsigned public Integer varUint;
-    public String varString;
+    public Integer varInteger;
+    @Unsigned public Long varUlong;
+    public Long varLong;
     public Float varFloat;
+    public Double varDouble;
+    public String varString;
     public OtherMessage varOther;
 
     public Integer[] varRepeatedInt;
     public OtherMessage[] varRepeatedOther;
-    
+
     @Required public String requiredField;
 }
--- Java/Test/src/Program.java
+++ Java/Test/src/Program.java
@@ -1,5 +1,6 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import com.conditionracer.protoserial.*;
@@ -15,9 +16,32 @@ public class Program
 			manager.RegisterMessageType(OtherMessage.class);
 			manager.RegisterMessageType(ParentMessage.class);
 			
+            MyMessage msg = new MyMessage();
+            msg.id = -1L;
+            msg.varIntegerNullable = null;
+            msg.varUshort = 1;
+            msg.varShort = -1;
+            msg.varUint = 9001;
+            msg.varInteger = -9001;
+            msg.varUlong = 9999999999999L;
+            msg.varLong = -9999999999999L;
+            msg.varFloat = 1.0f;
+            msg.varDouble = 9421844.442;
+            msg.varString = "Hello world";
+            msg.varOther = new OtherMessage();
+            msg.varOther.varXXX = 100L;
+            msg.requiredField = "";
+            msg.varRepeatedInt = new Integer[] { 1, 2, 3, 4, 5 };
+            msg.varRepeatedOther = new OtherMessage[] { new OtherMessage(), new OtherMessage() };
+            msg.varRepeatedOther[0].varXXX = 1000L;
+            msg.varRepeatedOther[1].varXXX = 2000L;
+            
+			FileOutputStream into = new FileOutputStream ( "output.txt" );
+			manager.Serialize ( msg, into );
+			into.close();
+			
 			FileInputStream from = new FileInputStream ( "output.txt" );
-			@SuppressWarnings("unused")
-			MyMessage msg = (MyMessage) manager.Deserialize ( from );
+			msg = (MyMessage) manager.Deserialize ( from );
 			from.close();
 		} catch (Exception e) {
 			e.printStackTrace();
